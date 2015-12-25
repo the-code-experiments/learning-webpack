847c7c8562f33b66841678244734146a1d9c8a4a
hegdeashwin <ashwin.hegde3@gmail.com>
2015-12-25 23:50:20 +0530

    Adds a work around for browser load integration issue

--- codes/example-10/webpack.config.js
+++ codes/example-10/webpack.config.js
@@ -1,4 +1,5 @@
-var OpenBrowserPlugin = require('open-browser-webpack-plugin');
+// var OpenBrowserPlugin = require('open-browser-webpack-plugin');
+var open = require("open");
 
 module.exports = {
   entry: './entry.js',
@@ -6,8 +7,13 @@ module.exports = {
     filename: 'bundle.js'
   },
   plugins: [
-    new OpenBrowserPlugin({
-      url: 'http://localhost:8080'
-    })
-  ]
+    // new OpenBrowserPlugin({
+    //   url: 'http://localhost:3000'
+    // }) // plugin is having issue https://github.com/baldore/open-browser-webpack-plugin/issues/4
+  ],
+  devServer: {
+    port: 3000 // setting webpack-dev-server port
+  }
 };
+
+open("http://localhost:3000", "firefox");
